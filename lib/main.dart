18aded663d0006af902288eaca6331606ef914bc
johnj-dev <68414617+johnj-dev@users.noreply.github.com>
2020-12-27 21:02:12 +0800

    Add Work Manager and Local Notification

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,58 @@
 import 'dart:io';
+import 'dart:convert';
+import 'dart:async';
 
 import 'package:amazon_s3_test/activities_buyer.dart';
 import 'package:amazon_s3_test/order_tracker.dart';
 import 'package:amazon_s3_test/performance_buyer.dart';
 import 'package:flutter/material.dart';
 import 'package:amazon_s3_test/policy_v3.dart';
+import 'package:workmanager/workmanager.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-void main() {
+//this is the name given to the background fetch
+const simplePeriodicTask = "testTask";
+// flutter local notification setup
+void showNotification(v, flp) async {
+  var android = AndroidNotificationDetails(
+      'channel id', 'channel NAME', 'CHANNEL DESCRIPTION',
+      priority: Priority.high, importance: Importance.max);
+  var iOS = IOSNotificationDetails();
+  var platform = NotificationDetails(android: android, iOS: iOS);
+  await flp.show(0, 'VeMoBro', '$v', platform,
+      payload: 'VIS \n $v');
+}
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Workmanager.initialize(callbackDispatcher, isInDebugMode: true); //to true if still in testing lev turn it to false whenever you are launching the app
+  await Workmanager.registerPeriodicTask("uniqueName", simplePeriodicTask,
+      existingWorkPolicy: ExistingWorkPolicy.replace,
+      frequency: Duration(minutes: 15),//when should it check the link
+      initialDelay: Duration(seconds: 5),//duration before showing the notification
+      constraints: Constraints(
+        networkType: NetworkType.connected,
+      ));
   runApp(MyApp());
 }
+
+void callbackDispatcher() {
+  Workmanager.executeTask((task, inputData) async {
+
+    FlutterLocalNotificationsPlugin flp = FlutterLocalNotificationsPlugin();
+    var android = AndroidInitializationSettings('@mipmap/ic_launcher');
+    var iOS = IOSInitializationSettings();
+    var initSetttings = InitializationSettings(android: android, iOS: iOS);
+    flp.initialize(initSetttings);
+    print(task);
+    print(inputData);
+
+    showNotification('TEST THIS NOTIFICATION', flp);
+
+    return Future.value(true);
+  });
+}
+
 
 class MyApp extends StatefulWidget {
   @override
